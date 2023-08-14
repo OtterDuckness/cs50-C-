48492f5869ca13fe4ffc2073a736677d378154fb
OtterDuckness <OtterDuckness@users.noreply.github.com>
2023-08-14 20:55:21 +0300

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -14,7 +14,7 @@ int main(void)
     {
         for (int x = (height); x > i; x--)
         {
-            printf(".");
+            printf(" ");
         }
         for (int j = 1; j <= i; j++)
         {
