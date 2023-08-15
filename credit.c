654c4bfa6a6803cb3f5583789a33fbd5a63ab080
OtterDuckness <OtterDuckness@users.noreply.github.com>
2023-08-15 21:44:32 +0300

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -3,40 +3,41 @@
 
 int main(void)
 {
-    //number input
-    long cnum = get_long ("Card number without spaces or slashes: ");
+    // number input
+    long cnum = get_long("Card number without spaces or slashes: ");
     long x = cnum;
 
-    //luhns voodoo
+    // luhns voodoo
     int a;
     int b;
     int y;
     int z;
     while (x > 0)
     {
-        a = (x % 100) / 10 * 2;  //evens from back to front
-        b = (x % 10);  //odds from back to front
-        if (a > 9)  //in case of a double-digit even
+        a = (x % 100) / 10 * 2; // evens from back to front
+        b = (x % 10);           // odds from back to front
+        if (a > 9)              // in case of a double-digit even
         {
             a = a % 10 + (a / 10) % 10;
         }
-        y = a + y;  //sum of evens
-        z = b + z;  //sum of odds
+        y = a + y; // sum of evens
+        z = b + z; // sum of odds
         x = x / 100;
     }
 
     int sum = y + z;
-    if (sum % 10 == 0)  //checking if valid
+    if (sum % 10 == 0) // checking if valid
     {
-        if (cnum / 10000000000000 == 34 || cnum / 10000000000000 == 37)        // amex
+        if (cnum / 10000000000000 == 34 || cnum / 10000000000000 == 37) // amex
         {
             printf("AMEX\n");
         }
-        else if (cnum / 100000000000000 == 51 || cnum / 100000000000000 == 52 || cnum / 100000000000000 == 53 || cnum / 100000000000000 == 54 || cnum / 100000000000000 == 55) // mastercard
+        else if (cnum / 100000000000000 == 51 || cnum / 100000000000000 == 52 || cnum / 100000000000000 == 53 ||
+                 cnum / 100000000000000 == 54 || cnum / 100000000000000 == 55) // mastercard
         {
             printf("MASTERCARD\n");
         }
-        else if (cnum / 1000000000000 == 4 || cnum / 1000000000000000 == 4)  // visa
+        else if (cnum / 1000000000000 == 4 || cnum / 1000000000000000 == 4) // visa
         {
             printf("VISA\n");
         }
