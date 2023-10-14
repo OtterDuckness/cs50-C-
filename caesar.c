b0249376a3fefbde190c5708dee4fb7f6a3e03de
OtterDuckness <OtterDuckness@users.noreply.github.com>
2023-10-14 19:35:51 +0300

    automated commit by submit50

--- caesar.c
+++ caesar.c
@@ -1,39 +1,35 @@
 #include <cs50.h>
-#include <string.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
 
 bool only_digits(string s);
 char rotate(char c, int n);
 
-
 int main(int argc, string argv[])
 {
-    if (argc != 2)
+    if (argc != 2)  //filters out multiple arguments
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    else if (only_digits(argv[1]) != 1)
+    else if (only_digits(argv[1]) != 1) //filters out non-numeric arguments
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
     int key = atoi(argv[1]);
     string plain = get_string("plaintext: ");
-    char cipher[strlen(plain)];
+    char cipher[strlen(plain)];  //"string cipher" doesnt work for some reason??
     printf("ciphertext: ");
-    for (int i = 0; i < strlen(plain); i++)
+    for (int i = 0; i < strlen(plain); i++)  //actual rotation
     {
         cipher[i] = rotate(plain[i], key);
         printf("%c", cipher[i]);
     }
     printf("\n");
-
-
 }
-
 
 bool only_digits(string s)
 {
@@ -41,7 +37,6 @@ bool only_digits(string s)
     {
         if (isdigit(s[i]))
         {
-
         }
         else
         {
@@ -54,15 +49,15 @@ bool only_digits(string s)
 
 char rotate(char c, int n)
 {
-    if (isalpha(c))
+    if (isalpha(c))  //filters out non-letters
     {
-        if (isupper(c))
+        if (isupper(c)) //for uppercase
         {
-           return (((c - 'A') + n)%26 + 'A');
+            return (((c - 'A') + n) % 26 + 'A');
         }
-        else
+        else  //for lowercase
         {
-            return (((c - 'a') + n)%26 + 'a');
+            return (((c - 'a') + n) % 26 + 'a');
         }
     }
     else
