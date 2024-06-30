ec312876d7b3ed6b99e2cc28310167c59eaa4dcf
OtterDuckness <OtterDuckness@users.noreply.github.com>
2024-06-30 12:52:17 +0200

    automated commit by submit50

--- plurality.c
+++ plurality.c
@@ -71,7 +71,7 @@ bool vote(string name)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
-            candidates[i].votes ++;
+            candidates[i].votes++;
             printf("vote %i\n", candidates[i].votes);
             return true;
         }
