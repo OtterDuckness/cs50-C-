187212ea6ebcd1f2a93f2349d33640fc4b9155e2
OtterDuckness <OtterDuckness@users.noreply.github.com>
2025-07-11 14:44:40 +0300

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -59,8 +59,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < (width/2); j++)
         {
             RGBTRIPLE temp= image[i][j];
-            image[i][j] = image[i][width - (j + 1)];
-            image[i][width - (j + 1)] = temp;
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
         }
     }
     return;
